537d2fc8ad3c0080ccec55344e7cedcf643ad942
Tazul Islam <linkedin52425@gmail.com>
2025-08-03 14:32:03 +0600

    delete

--- demo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    address = models.TextField()
-
-    def __str__(self):
-        return self.name
