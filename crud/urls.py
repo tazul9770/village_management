802fef43da79db2558a28b3156cc43d59b344117
Tazul Islam <linkedin52425@gmail.com>
2025-08-08 22:47:25 +0600

    first commit:

--- crud/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from crud.views import show_data, sign_in
-
-urlpatterns = [
-    path('show_data/', show_data, name='show_data'),
-    path('sign_in/', sign_in, name='sign_in')
-]
--- village_management/settings.py
+++ village_management/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "debug_toolbar",
-    'crud',
     'users'
 ]
 
