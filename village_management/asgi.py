8e755c7549140ddb3a43423fb99a1f1476d55148
Tazul Islam <linkedin52425@gmail.com>
2025-08-14 21:40:22 +0600

    again update

--- village_management/asgi.py
+++ village_management/asgi.py
@@ -1,9 +1,9 @@
-
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'village_management.settings')
 
 application = get_asgi_application()
 
+# Vercel expects a variable named `app`
+app = application
--- village_management/wsgi.py
+++ village_management/wsgi.py
@@ -1,10 +1,10 @@
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'village_management.settings')
 
+# Django WSGI application
 application = get_wsgi_application()
 
+# Vercel expects a variable named `app`
 app = application
