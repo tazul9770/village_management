67b00dde16e638806a8bf118de698468e494381a
Tazul Islam <linkedin52425@gmail.com>
2025-08-15 16:12:14 +0600

    settings update

--- village_management/settings.py
+++ village_management/settings.py
@@ -110,8 +110,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
