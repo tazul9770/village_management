882bd760191248d2597f18cdcd2d5e777a0c2f43
Tazul Islam <linkedin52425@gmail.com>
2025-08-15 15:12:30 +0600

    ready for deploy

--- village_management/settings.py
+++ village_management/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1','localhost']
  
